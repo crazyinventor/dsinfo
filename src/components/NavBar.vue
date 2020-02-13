c44117fe3cde9f3462f96db446256dfee0c474fb
crazyinventor <d4vid81@gmail.com>
2020-02-13 12:01:46 +0100

    Reducing max width of top navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,31 @@
 <template>
   <nav class="navbar is-fixed-top is-black" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <router-link to="/" class="navbar-item">
-        David Schneider
-      </router-link>
-      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"  @click="showNav = !showNav" :class="{ 'is-active': showNav }">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div class="navbar-menu" :class="{ 'is-active': showNav }">
-      <div class="navbar-start" @click="hideNavBar">
-        <router-link to="/about" class="navbar-item">
-          About
+    <div class="container">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          David Schneider
         </router-link>
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"  @click="showNav = !showNav" :class="{ 'is-active': showNav }">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-        <router-link to="/portfolio" class="navbar-item">
-          Portfolio
-        </router-link>
+      <div class="navbar-menu" :class="{ 'is-active': showNav }">
+        <div class="navbar-start" @click="hideNavBar">
+          <router-link to="/about" class="navbar-item">
+            About
+          </router-link>
 
-        <router-link to="/contact" class="navbar-item">
-          Contact
-        </router-link>
+          <router-link to="/portfolio" class="navbar-item">
+            Portfolio
+          </router-link>
+
+          <router-link to="/contact" class="navbar-item">
+            Contact
+          </router-link>
+        </div>
       </div>
     </div>
   </nav>
